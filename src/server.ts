3b1bc96fbc6ae03e157c370a6fa1ab75e62015b6
https-dre <diaso.andre@outlook.com>
2025-07-29 21:31:06 -0300

    style: remove 'opts'

--- src/server.ts
+++ src/server.ts
@@ -3,31 +3,31 @@ import fastifySocketIO from "fastify-socket.io";
 import { socketIoServer } from "./socket";
 
 const app = Fastify({
-	logger: true,
+  logger: true,
 });
 
 const run = async () => {
-	await app.register(fastifySocketIO);
+  await app.register(fastifySocketIO);
 
-	app.get("/", async () => {
-		return { hello: "world " };
-	});
+  app.get("/", async () => {
+    return { hello: "world " };
+  });
 
-	app.register(async (app, opts) => {
-		app.addHook("onReady", (done) => {
-			socketIoServer(app);
-			done();
-		});
-	});
+  app.register(async (app, _) => {
+    app.addHook("onReady", (done) => {
+      socketIoServer(app);
+      done();
+    });
+  });
 
-	app.listen({ port: 7750 }, (err, address) => {
-		if (err) {
-			app.log.error(err);
-			process.exit(1);
-		}
+  app.listen({ port: 7750 }, (err, address) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
 
-		console.log(`Server running: ${address}`);
-	});
+    console.log(`Server running: ${address}`);
+  });
 };
 
 run();
