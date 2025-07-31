5f0416e2df67a7dd55d6c26a6c56ea2063b0889a
https-dre <diaso.andre@outlook.com>
2025-07-31 20:36:51 -0300

    refactor: move '/' route to default router

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,15 @@
 import { FastifyInstance } from "fastify";
 import { MessageController } from "./controllers/Messages";
+import { logger } from "./log/logger";
 
 export const router = (app: FastifyInstance) => {
   const messageController = new MessageController();
 
+  app.get("/", (_, reply) => {
+    return reply.code(200).send("Realtime API running, go to /docs");
+  });
+
   app.put("/notify/all", messageController.SendToAll);
   app.put("/notify/on-client/", messageController.SendToOne);
+  logger.info("Routes defined.");
 };
