39882c0d8e9de39a0e333a2c891f34fb72037f04
https-dre <diaso.andre@outlook.com>
2025-07-31 19:58:25 -0300

    feat: add a basic logger config

---
+++ src/log/logger.ts
@@ -0,0 +1,12 @@
+import pino from "pino";
+
+export const logger = pino({
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      translateName: "SYS:standard",
+      ignore: "pid,hostname",
+    },
+  },
+});
