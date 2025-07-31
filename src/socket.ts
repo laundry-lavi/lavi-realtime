f6ef17290cad4fe0e0dc244850637bf89f27fabe
https-dre <diaso.andre@outlook.com>
2025-07-30 21:10:12 -0300

    style: format file

--- src/socket.ts
+++ src/socket.ts
@@ -1,14 +1,14 @@
 import { FastifyInstance } from "fastify";
 
 export async function socketIoServer(app: FastifyInstance) {
-	app.io.on("connection", (socket) => {
-		console.log("Novo cliente conectado:", socket.id);
+  app.io.on("connection", (socket) => {
+    console.log("Novo cliente conectado:", socket.id);
 
-		socket.on("mensagem", (mensagem) => {
-			console.log("Recebeu", mensagem);
+    socket.on("mensagem", (mensagem) => {
+      console.log("Recebeu", mensagem);
 
-			// Envia de volta para o cliente (ou para todos, se quiser)
-			app.io.emit("resposta", `Servidor recebeu: ${mensagem}`);
-		});
-	});
+      // Envia de volta para o cliente (ou para todos, se quiser)
+      app.io.emit("resposta", `Servidor recebeu: ${mensagem}`);
+    });
+  });
 }
