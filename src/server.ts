813dd34baabfec8312d8aa2ef445d65ccbae03df
André Dias <diaso.andre@outlook.com>
2025-08-01 17:01:03 +0000

    docs(api-reference): add scalar configs

--- src/server.ts
+++ src/server.ts
@@ -6,33 +6,55 @@ import { router } from "./router";
 import {
   validatorCompiler,
   serializerCompiler,
+  jsonSchemaTransform,
 } from "fastify-type-provider-zod";
 import { logger } from "./log/logger";
-import { db } from "./db/drizzle";
-import { verify_env } from "./config";
+import fastifySwagger from "@fastify/swagger";
 
 export const app = Fastify();
 
+app.register(fastifySocketIO);
+
+// @ts-ignore
+app.register(fastifySwagger, {
+  swagger: {
+    info: {
+      title: "Laví Realtime service",
+      description: "A socket.io based service"
+    }
+  },
+  transform: jsonSchemaTransform
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.addHook("onReady", (done) => {
+  logger.info("Loading socket.io...")
+  try { socketIoServer(app) }
+  catch (error) {
+    logger.fatal(error);
+  }
+  logger.info("socket.io loaded with success.")
+  done();
+});
+
+
+app.register(router);
+
 const run = async () => {
-  verify_env();
+  /*   verify_env();
+  
+    await app.register(fastifySocketIO);
+    logger.info("Testing database connection...");
+    await db.execute("SELECT 1");
+    logger.info("Database connection ok."); */
 
-  await app.register(fastifySocketIO);
-  logger.info("Testing database connection...");
-  await db.execute("SELECT 1");
-  logger.info("Database connection ok.");
 
-  app.setValidatorCompiler(validatorCompiler);
-  app.setSerializerCompiler(serializerCompiler);
-
-  app.register(async (app, _) => {
-    app.addHook("onReady", (done) => {
-      socketIoServer(app);
-      logger.info("Socket-IO initialized.");
-      done();
-    });
-  });
-
-  app.register(router);
+  // Setup for scalar api reference  
+  await app.register(import('@scalar/fastify-api-reference'), {
+    routePrefix: '/docs',
+  })
 
   const PORT = process.env.PORT ? process.env.PORT : "7750";
 
