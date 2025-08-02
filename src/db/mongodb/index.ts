6cdcca62d0cfa1504109c3958e32c1e5ba91671d
https-dre <diaso.andre@outlook.com>
2025-08-02 18:00:47 -0300

    feat: add mongoose startup config

---
+++ src/db/mongodb/index.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { logger } from "../../log/logger";
+
+if (!process.env.MONGO_URL) {
+  logger.fatal("MONGO_URL was not defined!");
+}
+
+export const connectDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL!);
+    logger.info("MongoDB connected");
+  } catch (error) {
+    logger.fatal("Error while connectig MongoDB: " + error);
+    process.exit(1);
+  }
+};
