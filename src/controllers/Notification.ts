9f9a99960a26ff7161b4471fc8d298fa5171cb55
https-dre <diaso.andre@outlook.com>
2025-08-02 17:57:54 -0300

    refactor: remove Message controller and create a new Notification controller

--- src/controllers/Notification.ts
+++ src/controllers/Notification.ts
@@ -1,7 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { app } from "../server";
 
-export class MessageController {
+export class NotificationController {
   public async SendToAll(
     req: FastifyRequest,
     reply: FastifyReply
@@ -16,18 +16,19 @@ export class MessageController {
     req: FastifyRequest,
     reply: FastifyReply
   ): Promise<void> {
-    const { title, content, id } = req.body as any;
+    const { title, content, id, type } = req.body as any;
 
     const allSockets = await app.io.fetchSockets();
 
     for (const socket of allSockets) {
       if (socket.data.id == id || socket.id == id) {
-        socket.emit("privateMessage", { title, content });
-        return reply.code(201).send("Mensagem enviada");
+        reply.code(204).send("Enviando notificação...");
+        socket.emit("privateNotification", { title, content, type });
+        return;
       }
     }
 
     // socket não encontrado
-    return reply.code(404).send("Socket not found");
+    return reply.code(404).send("Socket not found.");
   }
 }
