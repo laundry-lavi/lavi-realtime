ed7fd71bfd7ced07ef7c7005494fd1e620dc048b
https-dre <diaso.andre@outlook.com>
2025-08-02 18:06:26 -0300

    feat: add functions to verify env and connect to mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import Fastify from "fastify";
 import fastifySocketIO from "fastify-socket.io";
 import { socketIoServer } from "./socket";
-import { router } from "./router";
+import { router } from "./routes/index";
 
 import {
   validatorCompiler,
   serializerCompiler,
   jsonSchemaTransform,
 } from "fastify-type-provider-zod";
+
 import { logger } from "./log/logger";
 import fastifySwagger from "@fastify/swagger";
+
+import { verify_env } from "./config";
+import { connectDB } from "./db/mongodb/index";
 
 export const app = Fastify();
 
@@ -20,41 +24,41 @@ app.register(fastifySwagger, {
   swagger: {
     info: {
       title: "Laví Realtime service",
-      description: "A socket.io based service"
-    }
+      description: "A socket.io based service",
+    },
+    consumes: ["application/json"],
+    produces: ["application/json"],
   },
-  transform: jsonSchemaTransform
+  transform: jsonSchemaTransform,
 });
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.addHook("onReady", (done) => {
-  logger.info("Loading socket.io...")
-  try { socketIoServer(app) }
-  catch (error) {
+  logger.info("Loading socket.io...");
+  try {
+    socketIoServer(app);
+  } catch (error) {
     logger.fatal(error);
   }
-  logger.info("socket.io loaded with success.")
+  logger.info("socket.io loaded with success.");
   done();
 });
-
 
 app.register(router);
 
 const run = async () => {
-  /*   verify_env();
-  
-    await app.register(fastifySocketIO);
-    logger.info("Testing database connection...");
-    await db.execute("SELECT 1");
-    logger.info("Database connection ok."); */
+  verify_env();
 
+  logger.info("Connecting to MongoDb...");
+  connectDB();
+  logger.info("MongoDb connection Ok!");
 
-  // Setup for scalar api reference  
-  await app.register(import('@scalar/fastify-api-reference'), {
-    routePrefix: '/docs',
-  })
+  // Setup for scalar api reference
+  await app.register(import("@scalar/fastify-api-reference"), {
+    routePrefix: "/docs",
+  });
 
   const PORT = process.env.PORT ? process.env.PORT : "7750";
 
@@ -65,6 +69,7 @@ const run = async () => {
     }
 
     logger.info(`Server running at: ${address}`);
+    logger.info("See API Reference at /docs");
   });
 };
 
