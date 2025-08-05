86b3bcf8790d21415cf4a0bb961a541d9cd12d2e
André Dias <diaso.andre@outlook.com>
2025-08-05 12:39:06 +0000

    refactor: implement socket.io functions

--- src/controllers/Notification.ts
+++ src/controllers/Notification.ts
@@ -1,5 +1,5 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { app } from "../server";
+import { EmitToAll, FindSocketById } from "../functions/io-server-utils";
 
 export class NotificationController {
   public async SendToAll(
@@ -8,7 +8,8 @@ export class NotificationController {
   ): Promise<void> {
     const { title, content } = req.body as any;
 
-    app.io.emit("messageToAll", { title, content });
+    /* app.io.emit("messageToAll", { title, content }); */
+    EmitToAll("notificationToAll", { title, content });
     return reply.code(201).send("Mensagem enviada");
   }
 
@@ -18,17 +19,13 @@ export class NotificationController {
   ): Promise<void> {
     const { title, content, id, type } = req.body as any;
 
-    const allSockets = await app.io.fetchSockets();
+    const response = await FindSocketById(id);
 
-    for (const socket of allSockets) {
-      if (socket.data.id == id || socket.id == id) {
-        reply.code(204).send("Enviando notificação...");
-        socket.emit("privateNotification", { title, content, type });
-        return;
-      }
+    if (!response.data) {
+      return reply.code(404).send("Socket not found!");
     }
 
-    // socket não encontrado
-    return reply.code(404).send("Socket not found.");
+    reply.code(204).send();
+    response.data.emit("privateNotification", { title, content, type });
   }
 }
